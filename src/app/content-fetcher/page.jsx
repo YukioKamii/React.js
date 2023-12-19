0430c274b78e5fe3cdc6aaa532feef5ccd607b74
YukioKamii <yoyolc.2004@gmail.com>
2023-12-19 10:14:26 +0100

    adding content fetcher

---
+++ src/app/content-fetcher/page.jsx
@@ -0,0 +1,27 @@
+// "use client"
+
+// import { useEffect } from "react";
+
+// const ContentFetcher = () => {
+//     const URL = "https://foaas.dev";
+
+//     useEffect(async() => {
+//         const response = await fetch(`${URL}/operations`)
+//             .then((res) => res.json())
+//             .then((data) => console.log(data))
+//             .catch((err) => console.log(err));
+
+//             console.log(response);
+//     });
+
+//     return (
+//         <div>
+//             <p>Some Data</p>
+//         </div>
+//     );
+// };
+
+// export default ContentFetcher;
+
+
+
